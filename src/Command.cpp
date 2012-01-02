3da1f5d90c0cb407e42ac6ffa2425990322e3106
Jose Esteve <jose@joesfer.com>
2012-01-02 18:35:03 +0000

    [!] updated license terms to LGPL-3.0

--- src/Command.cpp
+++ src/Command.cpp
@@ -1,3 +1,10 @@
+/* 
+	================================================================================
+	Copyright (c) 2012, Jose Esteve. http://www.joesfer.com
+	This software is released under the LGPL-3.0 license: http://www.opensource.org/licenses/lgpl-3.0.html	
+	================================================================================
+*/
+
 #include "Command.h"
 #include <maya/MArgDatabase.h>
 #include <maya/MSelectionList.h>
--- src/common.h
+++ src/common.h
@@ -1,2 +1,8 @@
+/* 
+	================================================================================
+	Copyright (c) 2012, Jose Esteve. http://www.joesfer.com
+	This software is released under the LGPL-3.0 license: http://www.opensource.org/licenses/lgpl-3.0.html	
+	================================================================================
+*/
 
 #define GROWER_DISPLAY_DEBUG_INFO	0
